58d7ec98a894e0712f1d11e43db6f12b7b8435e4
shreetuition <info@shreetuitioncenter.com>
2021-10-09 22:51:41 +0530

    Done

---
+++ Core_PHP/index.php
@@ -0,0 +1,246 @@
+<?php 
+	
+	// 1. variables 
+	// 2. Statement
+	
+	// 3. Bodmas Rules 
+	
+	// $a = 6;
+
+	// $b = 8;
+
+	// echo ($a*$b) - $a;
+
+	// 1. SI => intrest Amount and payable amount
+	// 2. Area of triangle 	
+	// 3. Rec and Square 
+	// 4. .c to f 
+	// 5. swap two number without using 3 variable
+	// 6. swap two number without using with variable
+	// 7. Python Program to Convert Kilometers to Miles
+	// 8. Python Program to Find the Largest Among Three Numbers
+
+	
+	// $name = "kumar";
+
+	// echo $Name;
+	// die;
+
+	//$name = "";
+		
+	$number = 9;
+
+	// if ($number>0) {
+	// 	echo $number." Positive Number";
+	// }
+
+	// if ($number>0) {
+	// 	echo $number." Positive Number";
+	// } else {
+	// 	echo $number." Negative Number";
+	// }
+
+	// if ($number%2==0) {
+	// 	echo $number." Even Number";
+	// } else {
+	// 	echo $number." Odd Number";
+	// }
+
+
+	// if ($number>0) {
+	// 	if ($number%2==0) {
+	// 		echo $number." Even Number";
+	// 	} else {
+	// 		echo $number." Odd Number";
+	// 	}
+	// } else {
+	// 	echo "Invalid Number";
+	// }
+
+	$a = 16;
+
+	$b = 8;
+
+	$c = 9;
+
+	if ($a>$b && $a>$c) {
+		echo $a. " A";
+	} elseif ($b>$a && $b>$c) {
+		echo $b. " B";
+	} else {
+		echo $c. "C";
+	}
+
+
+	// echo isset($name);
+
+	// if (isset($name)) {
+	// 	echo "My Name is Sharvan";
+	// }
+
+	//The isset() function is an inbuilt function in PHP which checks whether a variable is set and is not NULL. This function also checks if a declared variable, array or array key has null value, if it does, isset() returns false, it returns true in all other possible cases.12-Aug-2021
+
+	//$name = "Kumar";
+		
+	//echo $name;
+
+	// $a = 5;
+
+	// $b = "5";
+
+	// if ($a===$b) {
+	// 	echo "Both number are same";
+	// } else {
+	// 	echo "Not same";
+	// }
+
+	// $number = 50;
+
+	// if ($number%2==0) {
+		
+	// 	$className = "alert-success";
+	// } else {
+	// 	$className = "alert-danger";
+	// }
+	
+	//A variable starts with the $ sign, followed by the name of the variable.
+	/* A variable name must start with a letter or the underscore character.
+	A variable name cannot start with a number.
+	A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+	*/
+
+	// $userName = "Kumar";
+
+	// $isStatus = true;
+
+	// $a = 4;
+
+	// var_dump($isStatus);
+
+	// echo $a;
+
+	// echo $userName;
+	// echo $userName;
+
+	// $userName
+	// $UserName 
+
+	// $user_name
+
+	// Camel Case 
+
+	// Pascal Case
+
+	// Snake Case
+
+	$firstName = "Sharvan";
+
+	$lastName = "Kumar";
+
+	echo "<h1>".$firstName." ".$lastName."</h1>";
+
+	$urlOfGoogle = "";
+
+	// $space = " ";
+
+	// echo $firstName.$space.$lastName;
+
+	// echo $firstName;
+	// echo $lastName;
+
+	// die();
+	// $a = 5;
+
+	// $b = (int) "5";
+
+	// //var_dump($b);
+
+	// echo $a + $b;
+
+	// echo "<br>";
+
+	// echo $a == $b;
+
+
+
+	// +
+	// -
+	// *
+	// /
+	// %
+	// >
+	// <
+	// >=
+	// <=
+	// ===
+	// ==
+	
+	// !=
+	
+	// && 
+
+	// OR
+
+	// !
+
+	// $a = 5;
+
+	// $b = 6;
+
+
+	// echo $a>20 OR $b>15;
+
+
+	// echo "<h1>Hello World</h1>";
+
+?>
+
+<h1><?php echo "Hello this is Kumar"; ?></h1>
+
+<h2><?php echo $firstName." ".$lastName;  ?></h2>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style type="text/css">
+    	/*h1 {
+    		background-color: <?php echo "green"; ?>;
+    	}*/
+    </style>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <div class="container">
+    	<div class="alert <?php echo $className; ?>">
+    		Alert
+    	</div>
+    </div>
+    
+
+
+    <script type="text/javascript">
+    	
+    
+    </script>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
