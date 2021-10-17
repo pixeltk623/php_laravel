a20e16efb0d3c7d365a197a4a04cf99f9e48edc1
shreetuition <info@shreetuitioncenter.com>
2021-10-17 13:05:57 +0530

    Done

---
+++ Core_PHP/loop.php
@@ -0,0 +1,173 @@
+<!-- 1. for loop =>  
+2. while loop =
+3. do while loop =>
+
+4. foreach loop (Array) -->
+
+<?php
+
+	// $i = 1;
+
+	// echo $i;
+
+	// echo "<br>";
+	// echo ++$i;
+
+
+	// echo "<br>";
+	// echo ++$i;
+
+
+	// echo "<br>";
+	// echo ++$i;
+
+	// echo "<br>";
+	// echo ++$i;
+
+
+	// echo "<br>";
+	// echo $i++;
+
+
+	// echo "<br>";
+	// echo $i;
+
+	// $i = 1;
+
+	// echo $i; // 1 
+
+	// echo "<br>";
+
+	// echo $i++; // 1
+
+	// echo "<br>";
+
+	// echo $i; // 2
+
+	// echo "<br>";
+
+	// echo $i; // 2
+
+	// echo "<br>";
+
+	// echo $i++; // 3
+
+	// echo $i;
+
+	// echo "<br>";
+	
+	// for ($i = 10; $i>0; $i--) { 
+		
+	// 	echo $i;
+	// 	echo "<br>";
+	// }
+
+	// $i = 1;
+	// while ($i <= 10) {
+	// 	echo $i;
+	// 	echo "<br>";
+
+	// 	$i++;
+	// }
+	
+	
+	// $i = 1;
+	// do {
+
+	// 	echo $i;
+	// 	echo "<br>";
+	// 	$i++;
+
+	// } while ($i <= 10);
+
+
+	// break 
+
+	// continue
+
+	// for ($i=1; $i <= 10; $i++) { 
+		
+	// 	if ($i==6) {
+	// 		break;
+	// 	}
+
+	// 	echo $i;
+	// 	echo "<br>";
+	// }
+
+	// for ($i=1; $i <= 10; $i++) { 
+		
+	// 	if ($i==4 AND $i == 8) {
+	// 		continue;
+	// 	}
+
+	// 	echo $i;
+	// 	echo "<br>";
+	// }
+	
+
+	// for ($i=1; $i <= 10; ++$i) { 
+		
+	// 	echo $i;
+
+	// 	echo "<br>";
+	// }
+	
+	
+	// $i = 1;
+	// while ($i<=10) {
+	// 	echo $i;
+
+	// 	echo "<br>";
+	// 	$i = $i + 2; //$i++ //$i =  $i+1
+	// }
+
+	// for ($i=1; $i <= 10; $i++) { 
+		
+	// 	if ($i%2!=0) {
+	// 		echo $i;
+	// 		echo "<br>";
+	// 	}
+
+	// }
+
+	
+	// $table = 5;
+
+	// for ($i=1; $i <= 10; $i++) { 
+		
+	// 	// echo $i;
+
+	// 	echo $table . " x ". $i . " = ". $table*$i;
+	// 	echo "<br>";
+	// }
+	
+	
+	// $sum = 0;
+	// for ($i=1; $i <= 10; $i++) { 
+		
+	// 	if ($i%2!=0) {
+	// 		$sum = $sum + $i;
+	// 	}
+		
+
+	// }
+
+	// echo $sum;
+	
+	// 1. Reverse Number => 123 => 321 
+	// 2. Count of digits => 123 => 3 
+	// 3. sum of digits => 123 => 1+2+3 = 5
+	// 4. palindrome number = > 121 => 121
+	// 5. Armstrong numbers => 153 => 1 +5 +3 
+	// 6. Prime No 
+	// 7. 1 -100 prime no 
+	// 8. 1 -1000 Armstrong numbers
+
+	// 9. 1 - 1000 palindrome number
+	// 10. factorial of number
+
+	
+
+
+?>
